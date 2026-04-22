e28cdc3455a3306fc4478e668da63694d631946a
putrastyo <me.putrasetyo@gmail.com>
2026-04-22 08:51:56 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
+});
