0724e7dfa7837119591468e903193c45255ed9c0
putrastyo <me.putrasetyo@gmail.com>
2026-05-30 12:59:17 +0700

    feat: authorization for facility

--- src/routes/facilityRoutes.js
+++ src/routes/facilityRoutes.js
@@ -8,13 +8,27 @@ import {
   updateFacility,
 } from "../controllers/facilityController.js";
 import { facilitySchema } from "../schemas/facilitySchema.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import isAdmin from "../middlewares/isAdminMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", validate(facilitySchema), createFacility);
+router.post(
+  "/",
+  authMiddleware,
+  isAdmin,
+  validate(facilitySchema),
+  createFacility,
+);
 router.get("/", getAllFacility);
 router.get("/:id", getFacilityById);
-router.put("/:id", validate(facilitySchema), updateFacility);
-router.delete("/:id", deleteFacility);
+router.put(
+  "/:id",
+  authMiddleware,
+  isAdmin,
+  validate(facilitySchema),
+  updateFacility,
+);
+router.delete("/:id", authMiddleware, isAdmin, deleteFacility);
 
 export default router;
